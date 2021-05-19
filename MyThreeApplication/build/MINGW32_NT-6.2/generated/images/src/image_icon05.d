539aed33ceee05cb0f03e67e7702c4a8d6590a5a
xinyue1007 <851910295@qq.com>
2021-05-19 17:12:25 +0800

    add three app custom Container MenuElement

---
+++ MyThreeApplication/build/MINGW32_NT-6.2/generated/images/src/image_icon05.d
@@ -0,0 +1,5 @@
+build/MINGW32_NT-6.2/generated/images/src/image_icon05.o: \
+ generated/images/src/image_icon05.cpp \
+ touchgfx/framework/include/touchgfx/hal/Config.hpp
+
+touchgfx/framework/include/touchgfx/hal/Config.hpp:
